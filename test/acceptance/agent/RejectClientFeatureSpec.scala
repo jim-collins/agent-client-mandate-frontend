ccbe7a40a8f347ec0911ac791684d6a8d5fb6d07
danPudwellOpencast <dan.pudwell@opencastsoftware.com>
2016-10-07 08:59:52 +0100

    simplified the feature

--- test/acceptance/agent/RejectClientFeatureSpec.scala
+++ test/acceptance/agent/RejectClientFeatureSpec.scala
@@ -16,24 +16,13 @@
 
 package acceptance.agent
 
-import java.util.UUID
-
-import org.joda.time.DateTime
 import org.jsoup.Jsoup
-import org.mockito.Matchers
-import org.scalatest.{BeforeAndAfterEach, FeatureSpec, GivenWhenThen}
 import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, FeatureSpec, GivenWhenThen}
 import org.scalatestplus.play.OneServerPerSuite
-import play.api.mvc.Result
-import play.api.test.Helpers._
-import uk.gov.hmrc.agentclientmandate.builders.{AuthBuilder, SessionBuilder}
-import uk.gov.hmrc.agentclientmandate.controllers.agent.RejectClientController
-import uk.gov.hmrc.agentclientmandate.models._
-import uk.gov.hmrc.agentclientmandate.service.AgentClientMandateService
-import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
-import uk.gov.hmrc.play.http.HeaderCarrier
-
-import scala.concurrent.Future
+import play.api.test.FakeRequest
+import uk.gov.hmrc.agentclientmandate.viewModelsAndForms.YesNoQuestionForm._
+import uk.gov.hmrc.agentclientmandate.views
 
 class RejectClientFeatureSpec extends FeatureSpec with OneServerPerSuite with MockitoSugar with BeforeAndAfterEach with GivenWhenThen{
 
@@ -43,37 +32,18 @@ class RejectClientFeatureSpec extends FeatureSpec with OneServerPerSuite with Mo
 
     scenario("user has visited the page") {
 
-      org.mockito.Mockito.when(mockAgentClientMandateService.fetchClientMandate(Matchers.any())(Matchers.any(), Matchers.any())) thenReturn Future.successful(Some(mandate))
-      viewWithAuthorisedAgent { result =>
-        Given("A user visits the page")
-        When("The user views the page")
-        val document = Jsoup.parse(contentAsString(result))
-        Then("The title should match - Are you sure you want to reject the request from ACME Limited?")
-        assert(document.title() === "Are you sure you want to reject the request from ACME Limited?")
-        And("The pre-header text is - Manage your ATED service")
-        assert(document.getElementById("pre-heading").text() === "Manage your ATED service")
-        And("The header text is - Are you sure you want to reject the request from ACME Limited?")
-        assert(document.getElementById("heading").text() === "Are you sure you want to reject the request from ACME Limited?")
-      }
+      Given("A user visits the page")
+      When("The user views the page")
+      implicit val request = FakeRequest()
+      val html = views.html.agent.rejectClient("", yesNoQuestionForm, "ACME Limited", "")
+      val document = Jsoup.parse(html.toString())
+      Then("The title should match - Are you sure you want to reject the request from ACME Limited?")
+      assert(document.title() === "Are you sure you want to reject the request from ACME Limited?")
+      And("The pre-header text is - Manage your ATED service")
+      assert(document.getElementById("pre-heading").text() === "Manage your ATED service")
+      And("The header text is - Are you sure you want to reject the request from ACME Limited?")
+      assert(document.getElementById("heading").text() === "Are you sure you want to reject the request from ACME Limited?")
+
     }
   }
-
-  def viewWithAuthorisedAgent(test: Future[Result] => Any) {
-    val userId = s"user-${UUID.randomUUID}"
-    implicit val hc: HeaderCarrier = HeaderCarrier()
-    implicit val user = AuthBuilder.createOrgAuthContext(userId, "name")
-    AuthBuilder.mockAuthorisedAgent(userId, mockAuthConnector)
-    val result = TestRejectClientController.view("ATED", "1").apply(SessionBuilder.buildRequestWithSession(userId))
-    test(result)
-  }
-
-  object TestRejectClientController extends RejectClientController {
-    override val authConnector = mockAuthConnector
-    override val acmService = mockAgentClientMandateService
-  }
-
-  val mockAgentClientMandateService = mock[AgentClientMandateService]
-  val mockAuthConnector = mock[AuthConnector]
-
-  val mandate = Mandate(id = "1", createdBy = User("credId", "agentName", Some("agentCode")), None, None, agentParty = Party("JARN123456", "agency name", PartyType.Organisation, ContactDetails("agent@agent.com", None)), clientParty = Some(Party("JARN123456", "ACME Limited", PartyType.Organisation, ContactDetails("client@client.com", None))), currentStatus = MandateStatus(Status.New, DateTime.now(), "credId"), statusHistory = Nil, Subscription(None, Service("ated", "ATED")))
 }
