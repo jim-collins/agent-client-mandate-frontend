45266712e097a7c51aa2e10f976501d8b7e1786c
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2017-11-03 13:20:18 +0000

    Fixed issue with screen-reader-text div being shown outside of page

--- public/javascript/clients.js
+++ public/javascript/clients.js
@@ -1,8 +1,7 @@
-var crTab = document.getElementById("clients")
 var prevUrl = document.referrer;
 
 if (prevUrl.includes("summary")) {
-    document.getElementById('clients-tab').focus();
+    document.getElementById("clients-tab").focus();
 }
 
 
